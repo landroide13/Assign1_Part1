ec1d9e1554c9536ce1b3018029ae66972b51a065
landroide13 <landrade1313@gmail.com>
2021-11-15 13:21:33 +1300

    start

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace part1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String[] City = { "Wellington", "Hamilton", "Taupo" };
+
+        }
+
+
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String[] City = { "Wellington", "Hamilton", "Taupo" };
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String[] Country = { "New Zealand", "Australia" };
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            string message = "";
+        }
+
+
+
+    }
+}
